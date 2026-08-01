9019edf6c04880565f5cb22b1b34308eea0fe08b
sayyadrehann <sayyadrehan5483@gmail.com>
2026-08-01 20:07:15 +0530

    Implement planet revolution using group rotation

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,14 +1,31 @@
 import { useRef } from "react";
-const planetRef = useRef();
+import { useFrame } from "@react-three/fiber";
+
 function Planet({ position, color, scale }) {
-  return (
+  const planetRef = useRef();
+  const orbitRef = useRef();
+
+ 
+  useFrame(() => {
+  if (planetRef.current) {
+    planetRef.current.rotation.y += 0.02;
+  }
+
+  if (orbitRef.current) {
+    orbitRef.current.rotation.y += 0.005;
+  }
+});
+return (
+  <group ref={orbitRef}>
     <mesh
-     ref={planetRef}
-     position={position} scale={scale}>
+      ref={planetRef}
+      position={position}
+      scale={scale}
+    >
       <sphereGeometry />
       <meshStandardMaterial color={color} />
     </mesh>
-  );
-}
+  </group>
+);
 
 export default Planet;
