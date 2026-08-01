e40cacc7d37e487dd0ef62f0b581584f0b0161c5
sayyadrehan09 <sayyadrehan507@gmail.com>
2026-08-02 00:34:33 +0530

    Add configurable rotation and orbit speeds to Planet component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Sun from "./components/Sun";
 import Planet from "./components/Planet";
-
+  
 function App() {
   return (
     <Canvas>
@@ -10,11 +10,39 @@ function App() {
 
   <Sun position={[0, 0, 0]} />
 
+  
+  //Mercury 
   <Planet
-    position={[4, 0, 0]}
-    color="blue"
-    scale={0.5}
-  />
+  position={[2,0,0]}
+  color="gray"
+  scale={0.2}
+  rotationSpeed={0.01}
+  orbitSpeed={0.03}
+/>
+//venus
+<Planet
+  position={[3,0,0]}
+  color="yellow"
+  scale={0.35}
+  rotationSpeed={0.005}
+  orbitSpeed={0.02}
+/>
+//Earth
+<Planet
+  position={[4,0,0]}
+  color="blue"
+  scale={0.5}
+  rotationSpeed={0.02}
+  orbitSpeed={0.01}
+/>
+//Mars
+<Planet
+  position={[6,0,0]}
+  color="red"
+  scale={0.4}
+  rotationSpeed={0.015}
+  orbitSpeed={0.008}
+/>
 
   <OrbitControls />
 </Canvas>
--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,18 +1,24 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
-function Planet({ position, color, scale }) {
+function Planet({
+  position,
+  color,
+  scale,
+  rotationSpeed,
+  orbitSpeed,
+}) {
   const planetRef = useRef();
   const orbitRef = useRef();
 
  
   useFrame(() => {
   if (planetRef.current) {
-    planetRef.current.rotation.y += 0.02;
+  planetRef.current.rotation.y+=rotationSpeed;
   }
 
   if (orbitRef.current) {
-    orbitRef.current.rotation.y += 0.005;
+    orbitRef.current.rotation.y += orbitSpeed;
   }
 });
 return (
@@ -27,5 +33,5 @@ return (
     </mesh>
   </group>
 );
-
+}
 export default Planet;
