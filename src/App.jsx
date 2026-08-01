015579f429d124ec253551f781e79b4f3e943088
sayyadrehan09 <sayyadrehan507@gmail.com>
2026-08-02 00:53:00 +0530

    Render planets dynamically using data and map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Sun from "./components/Sun";
 import Planet from "./components/Planet";
+import planets from "./data/planets";
   
 function App() {
   return (
@@ -10,39 +11,17 @@ function App() {
 
   <Sun position={[0, 0, 0]} />
 
-  
-  //Mercury 
+  {planets.map((planet) => (
   <Planet
-  position={[2,0,0]}
-  color="gray"
-  scale={0.2}
-  rotationSpeed={0.01}
-  orbitSpeed={0.03}
-/>
-//venus
-<Planet
-  position={[3,0,0]}
-  color="yellow"
-  scale={0.35}
-  rotationSpeed={0.005}
-  orbitSpeed={0.02}
-/>
-//Earth
-<Planet
-  position={[4,0,0]}
-  color="blue"
-  scale={0.5}
-  rotationSpeed={0.02}
-  orbitSpeed={0.01}
-/>
-//Mars
-<Planet
-  position={[6,0,0]}
-  color="red"
-  scale={0.4}
-  rotationSpeed={0.015}
-  orbitSpeed={0.008}
-/>
+    key={planet.name}
+    position={planet.position}
+    color={planet.color}
+    scale={planet.scale}
+    rotationSpeed={planet.rotationSpeed}
+    orbitSpeed={planet.orbitSpeed}
+  />
+))}
+
 
   <OrbitControls />
 </Canvas>
---
+++ src/data/planets.js
@@ -0,0 +1,36 @@
+const planets = [
+  {
+    name: "Mercury",
+    position: [2, 0, 0],
+    color: "gray",
+    scale: 0.2,
+    rotationSpeed: 0.01,
+    orbitSpeed: 0.03,
+  },
+  {
+    name: "Venus",
+    position: [3, 0, 0],
+    color: "yellow",
+    scale: 0.35,
+    rotationSpeed: 0.005,
+    orbitSpeed: 0.02,
+  },
+  {
+    name: "Earth",
+    position: [4, 0, 0],
+    color: "blue",
+    scale: 0.5,
+    rotationSpeed: 0.02,
+    orbitSpeed: 0.01,
+  },
+  {
+    name: "Mars",
+    position: [6, 0, 0],
+    color: "red",
+    scale: 0.4,
+    rotationSpeed: 0.015,
+    orbitSpeed: 0.008,
+  },
+];
+
+export default planets;
