d81c5d0b71aad31800feb614249d9220b842fdf4
sayyadrehan09 <sayyadrehan507@gmail.com>
2026-08-18 23:16:42 +0530

    Add textured 3D Sun

--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -1,8 +1,34 @@
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
 function Sun({ position }) {
+  const sunRef = useRef();
+
+  const sunTexture = useLoader(
+    TextureLoader,
+    "/textures/sun.jpg"
+  );
+
+  useFrame(() => {
+    if (sunRef.current) {
+      sunRef.current.rotation.y += 0.001;
+    }
+  });
+
   return (
-    <mesh position={position}>
-      <sphereGeometry />
-      <meshStandardMaterial color="orange" />
+    <mesh
+      ref={sunRef}
+      position={position}
+    >
+      <sphereGeometry args={[1.5, 64, 64]} />
+
+      <meshStandardMaterial
+        map={sunTexture}
+        emissiveMap={sunTexture}
+        emissiveIntensity={1.5}
+      />
     </mesh>
   );
 }
